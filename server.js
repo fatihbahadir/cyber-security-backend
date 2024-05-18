452e09b81fbc490fa784286208b203ebe11d01a7
fatihbahadir <hunkarhyme@gmail.com>
2024-05-18 11:10:04 +0300

    JWT

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+const verifyJWT = require('./middleware/verifyJWT');
+const cookieParser = require('cookie-parser');
+const credentials = require('./middleware/credentials');
+const PORT = process.env.PORT || 3500;
+
+// custom middleware logger
+app.use(logger);
+
+// handle options credentials check - before CORS! and fetch cookies credentials requirements
+app.use(credentials);
+
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+
+// built-in middleware to handle urlencoded form data
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json
+app.use(express.json());
+
+// middleware for cookies
+app.use(cookieParser());
+
+app.use('/register', require('./routes/register'));
+app.use('/auth', require('./routes/auth'));
+app.use('/refresh', require('./routes/refresh'));
+app.use('/logout', require('./routes/logout'));
+
+
+app.use(verifyJWT);
+app.use('/employees', require('./routes/api/employees'));
+
+// app.use('/)
+app.get('/', (req, res) => {
+    res.send("Hello world!")
+})
+
+app.use(errorHandler);
+
+app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`))
